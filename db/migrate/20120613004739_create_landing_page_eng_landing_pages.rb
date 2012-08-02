e3b66e62a51a3d421e061644aeb8d1b26b98a4ce
Martin Logan <martinjlogan@Martins-MacBook-Air.local>
2012-08-01 22:01:03 -0500

    removed the tagline field and added metakeywords

--- db/migrate/20120613004739_create_landing_page_eng_landing_pages.rb
+++ db/migrate/20120613004739_create_landing_page_eng_landing_pages.rb
@@ -1,12 +1,11 @@
 class CreateLandingPageEngLandingPages < ActiveRecord::Migration
 	def change
 		create_table :landing_page_eng_landing_pages do |t|
-			t.text :meta_description
-			t.text :title
-			t.text :tagline
-			t.text :body
-			t.string :call_to_action_url
 			t.string :slug
+			t.text :meta_description
+			t.text :meta_keywords
+			t.text :title
+			t.text :body
 
 			t.timestamps
 		end
